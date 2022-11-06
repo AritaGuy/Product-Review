d7472f16460ec99fd5114c0bb8225755d4abd7ff
Sam Arita <arita.sam@student.moringaschool.com>
2022-11-07 02:47:27 +0300

    completed assignment

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,21 @@
 class Product < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+      Reviews.create(user_id:user.id, star_rating:star_rating, comment:comment, product_id:product.id)
+    end
+
+    def print_all_reviews
+        self.reviews.collect do |product|
+            "Review for #{self.name} by #{product.user.name}:#{product.star_rating}. #{product.comment}" 
+        end
+    end
+
+    def average_rating
+      arr = self.reviews.collect do |product|
+         product.star_rating
+        end
+       arr.sum/arr.size
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews
+
+    def favorite_product
+        # self.reviews.collect do |t|
+        #     t.star_rating
+        # end.max()
+        self.reviews.order(:star_rating).last
+    end
+
+    def remove_reviews(product)
+        # Review.destroy(products.name == product)
+        self.reviews.where(product: product).destroy_all
+    end
 end
